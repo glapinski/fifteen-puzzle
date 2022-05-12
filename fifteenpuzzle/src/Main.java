a3f8f156b385bdc3f7a8bad0aec697ebf7455398
glapinski <236534@edu.p.lodz.pl>
2022-05-13 00:33:35 +0200

    base classes added

--- fifteenpuzzle/src/Main.java
+++ fifteenpuzzle/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String strategy = args[0];
+        String param = args[1];
+        String puzzle = args[2];
+        String solution = args[3];
+        String stats = args[4];
     }
 }
