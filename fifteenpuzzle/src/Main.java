52ce703f75412042885c182d86ae4b5d94904925
glapinski <236534@edu.p.lodz.pl>
2022-05-13 02:40:27 +0200

    powoli do przodu

--- fifteenpuzzle/src/Main.java
+++ fifteenpuzzle/src/Main.java
@@ -1,9 +1,35 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        String strategy = args[0];
-        String param = args[1];
-        String puzzle = args[2];
-        String solution = args[3];
-        String stats = args[4];
+        //String strategy = args[0];
+        String strategy = "bfs";
+        //String param = args[1];
+        String param = "LRUD";
+        //String puzzle = args[2];
+        String puzzle = "puzzles/4x4_04_00007.txt";
+        //String solution = args[3];
+        String solution = "solution.txt";
+        //String stats = args[4];
+        String stats = "stats.txt";
+
+        Bfs bfs = new Bfs();
+
+        int[][] finalBoard = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 0}
+        };
+        Board puzzleToSolve = new Board(puzzle);
+        Board solved = new Board(finalBoard);
+
+        switch(strategy) {
+            case "bfs":
+                bfs.bfs(puzzleToSolve, solved, param);
+            case "dfs":
+
+            case "astr":
+        }
     }
 }
